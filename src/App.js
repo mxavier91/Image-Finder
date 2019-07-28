3d2ffb1a225d1edee3ce6c7991382a0fe484a4b8
Malcolm Thomas <mxavier17@gmail.com>
2019-07-28 17:12:57 -0400

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './Components/navbar/NavBar';
+import Search from './Components/search/Search';
+import './App.css';
+
+function App() {
+  return (
+    <MuiThemeProvider>
+      <div>
+        <NavBar />;
+        <Search />
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/Components/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => {
+  return (
+    <div>
+      <AppBar title='PixaBay Image Finder' />
+    </div>
+  );
+};
+
+export default NavBar;
