eddc57094ca969352b21c94e2a5d142fe7b400d7
Malcolm Thomas <mxavier17@gmail.com>
2019-07-28 19:07:11 -0400

    Finished Project. Completed the ImageResults component. A user can search for a image and when they click on the zoom in icon, a modal will pop up. There will also be a close button in the bottom corner.

--- src/Components/search/Search.js
+++ src/Components/search/Search.js
@@ -16,15 +16,21 @@ class Search extends Component {
 
   // A METHOD THAT SHOWS THE IMAGE RESULTS WHEN TYPED IN THE SEARCH BAR
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then(res => this.setState({ images: res.data.hits }))
-        .catch(err => console.log(err));
+    const val = e.target.value;
+
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
     });
   };
 
